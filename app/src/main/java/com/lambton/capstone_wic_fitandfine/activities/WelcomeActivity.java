2a060a88bbfd526be258926292c2f575a0c02793
monikasharma26 <monikasharma26081991@gmail.com>
2020-08-20 17:19:29 -0400

    [Monika]  welcome activity added

---
+++ app/src/main/java/com/lambton/capstone_wic_fitandfine/activities/WelcomeActivity.java
@@ -0,0 +1,45 @@
+package com.lambton.capstone_wic_fitandfine.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.lambton.capstone_wic_fitandfine.R;
+
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_welcome);
+            initUIViews();
+        }
+
+        private void initUIViews(){
+            findViewById(R.id.activity_welcome_get_started).setOnClickListener(this);
+            findViewById(R.id.activity_welcome_log_in).setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.activity_welcome_get_started:
+                    Log.d("WelcomeActivity", "CLICKED");
+                    startedButtonClicked();
+                    break;
+                case R.id.activity_welcome_log_in:
+                 //   NavUtils.navigateUpFromSameTask(this);
+                    startActivity(new Intent(this,
+                            LoginActivity.class));
+            }
+        }
+
+        private void startedButtonClicked() {
+            startActivity(new Intent(this,
+                    RegistrationFormActivity.class));
+        }
+    }
