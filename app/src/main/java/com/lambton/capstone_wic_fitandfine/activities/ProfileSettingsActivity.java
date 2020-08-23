36f539ad9ecbaebc1d14eade2835fa67e5200af4
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-08-23 14:02:07 -0400

    Profile settings screen xml added

---
+++ app/src/main/java/com/lambton/capstone_wic_fitandfine/activities/ProfileSettingsActivity.java
@@ -0,0 +1,16 @@
+package com.lambton.capstone_wic_fitandfine.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lambton.capstone_wic_fitandfine.R;
+
+public class ProfileSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_settings);
+    }
+}
