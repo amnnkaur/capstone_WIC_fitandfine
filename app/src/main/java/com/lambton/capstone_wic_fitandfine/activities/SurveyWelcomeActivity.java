9d79b2d4ad06329a8f5e5c90c02ac2ac347403cc
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-08-28 13:55:20 -0400

    Welcome image updated

--- app/src/main/java/com/lambton/capstone_wic_fitandfine/activities/SurveyWelcomeActivity.java
+++ app/src/main/java/com/lambton/capstone_wic_fitandfine/activities/SurveyWelcomeActivity.java
@@ -1,8 +1,12 @@
 package com.lambton.capstone_wic_fitandfine.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.lambton.capstone_wic_fitandfine.R;
 
@@ -12,5 +16,26 @@ public class SurveyWelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_welcome);
+
+        setToolBar();
+        Button take_survey_btn = (Button) findViewById(R.id.btn_survey);
+        take_survey_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                Intent myIntent = new Intent(SurveyWelcomeActivity.this,
+                        SurveyStartActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
-}
+
+    private void setToolBar() {
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(getResources().getString(R.string.title_surveys));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    }
