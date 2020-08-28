de7b7f4e4321bf478a148937edeac8fc27aa4c2a
monikasharma26 <monikasharma26081991@gmail.com>
2020-08-28 01:38:55 -0400

    [Monika] Icons updated

--- app/src/main/java/com/lambton/capstone_wic_fitandfine/activities/ProfileSettingsActivity.java
+++ app/src/main/java/com/lambton/capstone_wic_fitandfine/activities/ProfileSettingsActivity.java
@@ -12,9 +12,9 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.lambton.capstone_wic_fitandfine.R;
+import com.lambton.capstone_wic_fitandfine.models.SettingsData;
 
-public class ProfileSettingsActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener,View.OnClickListener{
-
+public class ProfileSettingsActivity extends AppCompatActivity {
     private TextView TurnPushNotification;
     private SwitchCompat switch_email;
     private SwitchCompat switch_text;
@@ -35,35 +35,14 @@ public class ProfileSettingsActivity extends AppCompatActivity implements Compou
             switch_email = (SwitchCompat) findViewById(R.id.switch_email);
             switch_text = (SwitchCompat) findViewById(R.id.switch_text);
             switch_touchid = (SwitchCompat) findViewById(R.id.switch_touchid);
-            switch_email.setOnCheckedChangeListener(this);
-            switch_text.setOnCheckedChangeListener(this);
-            switch_touchid.setOnCheckedChangeListener(this);
-            text_standard.setOnClickListener(this);
-            text_metric.setOnClickListener(this);
-            TurnPushNotification.setOnClickListener(this);
+
+
 
         }
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
-            getMenuInflater().inflate(R.menu.menu_done, menu);
+           // getMenuInflater().inflate(R.menu.menu_done, menu);
             return true;
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-
-            switch (item.getItemId()) {
-                case R.id.menuDone:
-                    mPresenter.setProfileSettingValues(settingsData);
-
-              /*  Intent dataIntent = new Intent();
-                dataIntent.putExtra(Intent.EXTRA_TEXT, mPresenter.getAllergyData());
-                setResult(RESULT_OK, dataIntent);
-                finish();*/
-                    return true;
-                default:
-                    return super.onOptionsItemSelected(item);
-            }
         }
 
 
@@ -75,106 +54,5 @@ public class ProfileSettingsActivity extends AppCompatActivity implements Compou
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }*/
 
-        @Override
-        public void updateProfileSetting(SettingsData settingsData){
-            this.settingsData = settingsData;
-            if (settingsData.getPushNotificationEnabled() == 1)
-                TurnPushNotification.setTextColor(ContextCompat.getColor(this, R.color.blue));
-            else
-                TurnPushNotification.setTextColor(ContextCompat.getColor(this, R.color.dark_gray));
 
-            if (settingsData.getEmailNotificationEnabled() == 1)
-                switch_email.setChecked(true);
-            else
-                switch_email.setChecked(false);
-
-
-            if (settingsData.getSmsNotificationEnabled() == 1)
-                switch_text.setChecked(true);
-            else
-                switch_text.setChecked(false);
-
-            if (settingsData.getMeasurementType().equalsIgnoreCase("standard")) {
-                text_standard.setBackgroundColor(ContextCompat.getColor(this, R.color.blue));
-                text_standard.setTextColor(ContextCompat.getColor(this, R.color.white));
-
-                text_metric.setBackgroundColor(ContextCompat.getColor(this, R.color.settings_back));
-                text_metric.setTextColor(ContextCompat.getColor(this, R.color.dark_gray));
-            } else {
-                text_metric.setBackgroundColor(ContextCompat.getColor(this, R.color.blue));
-                text_metric.setTextColor(ContextCompat.getColor(this, R.color.white));
-
-                text_standard.setBackgroundColor(ContextCompat.getColor(this, R.color.settings_back));
-                text_standard.setTextColor(ContextCompat.getColor(this, R.color.dark_gray));
-            }
-
-
-        }
-
-    }
-
-    @Override
-    public void onClick(View view) {
-
-        switch (view.getId()){
-            case R.id.text_standard:
-                this.settingsData.setMeasurementType("standard");
-                text_standard.setBackgroundColor(ContextCompat.getColor(this, R.color.blue));
-                text_standard.setTextColor(ContextCompat.getColor(this, R.color.white));
-
-                text_metric.setBackgroundColor(ContextCompat.getColor(this, R.color.settings_back));
-                text_metric.setTextColor(ContextCompat.getColor(this, R.color.dark_gray));
-
-                break;
-            case R.id.text_metric:
-                this.settingsData.setMeasurementType("metric");
-
-                text_metric.setBackgroundColor(ContextCompat.getColor(this, R.color.blue));
-                text_metric.setTextColor(ContextCompat.getColor(this, R.color.white));
-
-                text_standard.setBackgroundColor(ContextCompat.getColor(this, R.color.settings_back));
-                text_standard.setTextColor(ContextCompat.getColor(this, R.color.dark_gray));
-
-                break;
-            case R.id.TurnPushNotification:
-                if(this.settingsData.getPushNotificationEnabled()==1){
-                    this.settingsData.setPushNotificationEnabled(0);
-                    TurnPushNotification.setTextColor(ContextCompat.getColor(this, R.color.dark_gray));
-                }
-                else {
-                    this.settingsData.setPushNotificationEnabled(1);
-                    TurnPushNotification.setTextColor(ContextCompat.getColor(this, R.color.blue));
-                }
-
-                break;
-        }
-
-    }
-
-    @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-
-        switch (compoundButton.getId()){
-            case R.id.switch_email:
-                if(b)
-                    this.settingsData.setEmailNotificationEnabled(1);
-                else
-                    this.settingsData.setEmailNotificationEnabled(0);
-                break;
-            case R.id.switch_touchid:
-               /* if(b)
-                    this.settingsData.setEmailNotificationEnabled(1);
-                else
-                    this.settingsData.setEmailNotificationEnabled(0);*/
-                break;
-            case R.id.switch_text:
-                if(b)
-                    this.settingsData.setSmsNotificationEnabled(1);
-                else
-                    this.settingsData.setSmsNotificationEnabled(0);
-                break;
-        }
-
-
-    }
 }
