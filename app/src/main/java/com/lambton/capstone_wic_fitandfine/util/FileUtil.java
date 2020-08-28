26631ee9573687e5c44417e6803b2bbb93b85585
monikasharma26 <monikasharma26081991@gmail.com>
2020-08-28 01:21:37 -0400

    [Monika] FileUtil class added

---
+++ app/src/main/java/com/lambton/capstone_wic_fitandfine/util/FileUtil.java
@@ -0,0 +1,25 @@
+package com.lambton.capstone_wic_fitandfine.util;
+
+import android.content.Context;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class FileUtil {
+
+    public static String readJSONFromAsset(Context context, String fileName){
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open(fileName);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return json;
+    }
+}
