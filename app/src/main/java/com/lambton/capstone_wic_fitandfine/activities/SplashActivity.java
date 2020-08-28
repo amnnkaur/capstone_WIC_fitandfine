1abf18129e2a7888920adae131d2f14c0936db43
monikasharma26 <monikasharma26081991@gmail.com>
2020-08-28 13:38:29 -0400

    [Monika] Splash Screen updated

--- app/src/main/java/com/lambton/capstone_wic_fitandfine/activities/SplashActivity.java
+++ app/src/main/java/com/lambton/capstone_wic_fitandfine/activities/SplashActivity.java
@@ -1,16 +1,31 @@
 package com.lambton.capstone_wic_fitandfine.activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.lambton.capstone_wic_fitandfine.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final int duration = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent;
+                intent = new Intent(SplashActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, duration);
     }
 }
